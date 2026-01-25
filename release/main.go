a5b9bf9265586cc2a8265aa87993915d08e8d693
MA3CIN <marcin.zet@onet.pl>
2026-01-25 17:55:16 +0000

    Compose: working go service, grafana, prometheus

---
+++ release/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+func main() {
+	testMetric := promauto.NewCounter(prometheus.CounterOpts{
+		Name: "test_metric",
+		Help: "A simple metric that increments every 10 seconds",
+	})
+
+	go func() {
+		for {
+			testMetric.Inc()
+			fmt.Println("Metric incremented at:", time.Now().Format("15:04:05"))
+			time.Sleep(10 * time.Second)
+		}
+	}()
+
+	http.Handle("/metrics", promhttp.Handler())
+
+	fmt.Println("Server starting on :8080/metrics")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
